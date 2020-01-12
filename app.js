132d08a29c4f316e5826a9104aa4797e7837cd6d
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-13 12:23:08 +1300

    create UI variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskinput = document.querySelector("#task");

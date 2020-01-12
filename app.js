1561ad677383a7a490b9441f3caab03ea0f3cc22
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-13 12:56:26 +1300

    create a function that add a new task that includes the creation of a new li element and its text node

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
-const taskinput = document.querySelector("#task");
+const taskInput = document.querySelector("#task");
+
+loadEventListeners();
+
+loadEventListeners = () => {
+  form.addEventListener("submit", addTask);
+};
+
+addTask = e => {
+  if (taskInput.value === "") {
+    alert("Please, add a task");
+  }
+
+  const li = document.createElement("li");
+  li.className = "colletion-ietm";
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  e.preventDefault();
+};

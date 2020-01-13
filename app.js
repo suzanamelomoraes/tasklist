10aa802bd8768ebbeb9dc3e2c7bcce32027c4bc2
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-13 16:19:19 +1300

    create function that check if the task to be removed by the user request is inside the local storage and removes it if has a positive answer

--- app.js
+++ app.js
@@ -82,6 +82,22 @@ function removeTask(e) {
   }
 }
 
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function(task, index) {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+}
+
 function clearTasks() {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);

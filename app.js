dfc2e9273edd7107a8c9e6716f7be582c384b873
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-13 16:03:20 +1300

    create function that get the tasks from the local storage and includes create an array and li to display the tasks stored to the user

--- app.js
+++ app.js
@@ -14,6 +14,28 @@ function loadEventListeners() {
   filter.addEventListener("keyup", filterTasks);
 }
 
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function(task) {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(task));
+
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = "<i class='fa fa-remove'></i>";
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+  });
+}
+
 function addTask(e) {
   if (taskInput.value === "") {
     alert("Please, add a task");
